fe5e6dab3da38ecd17a185fd9517613c8632f938
sangnguyenms <44984007+sangnguyenms@users.noreply.github.com>
2020-06-07 14:47:56 +0700

    Add files via upload

---
+++ AppData/UserFriend.java
@@ -0,0 +1,22 @@
+package AppData;
+
+public class UserFriend {
+	private String user="";
+	private String userFriend="";
+	public void setFriend(String userName,String userFriendName) {
+		user=userName;
+		userFriend=userFriendName;
+	}
+	public String getUser() {
+		return user;
+	}
+	public String getUserFriend() {
+		return userFriend;
+	}
+	public void setUser(String userName) {
+		user=userName;
+	}
+	public void setUserFriend(String userFriendNamee) {
+		userFriend=userFriendNamee;
+	}
+}
