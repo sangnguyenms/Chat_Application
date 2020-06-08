b77f0995166b24a0a1941204b27d1b6d6a4d91c7
sangnguyenms <66183577+ngphuocsang@users.noreply.github.com>
2020-06-08 14:33:26 +0700

    Update ServerGUI.java

--- server/ServerGUI.java
+++ server/ServerGUI.java
@@ -1,220 +1,147 @@
-package server;
-
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
-import javax.swing.JButton;
-import java.awt.TextArea;
-import java.awt.Font;
-
-import java.awt.Color;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.UIManager;
 
+
 public class ServerGUI {
-
-	public static int port = 8080;
-	private JFrame frmServerMangement;
-	private JTextField txtIP, txtPort;
-	private JLabel lblStatus;
+	public static short port = 8080;
+	private JFrame frServer;
+	private JTextField txtIP;
 	private static TextArea txtMessage;
-	public static JLabel lblUserOnline;
+	private JLabel lbStates;
+	private static JLabel lbUserOnl;
 	Server server;
-
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		try {
-			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
-            {
-                if ("Nimbus".equals(info.getName()))
-                {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-		} catch(Exception e) {
-			  System.out.println("Error setting native LAF: " + e);
+			for(javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".contentEquals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Nimbus is not available, error: " + e);
 		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ServerGUI window = new ServerGUI();
-					window.frmServerMangement.setVisible(true);
+					ServerGUI serverWindow = new ServerGUI();
+					serverWindow.frServer.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	public ServerGUI() {
-		initialize();
-	}
 	
-	public static String getLabelUserOnline() {
-		return lblUserOnline.getText();
-	} 
-	public static void updateMessage(String msg) {
-		txtMessage.append(msg + "\n");
-	}
-
-	public static void updateNumberClient() {
-		int number = Integer.parseInt(lblUserOnline.getText());
-		lblUserOnline.setText(Integer.toString(number + 1)); 
-	}
-	
-	public static void decreaseNumberClient() {
-		int number = Integer.parseInt(lblUserOnline.getText());
-		lblUserOnline.setText(Integer.toString(number - 1));
- 
-	}
-
 	private void initialize() {
-		frmServerMangement = new JFrame();
-		frmServerMangement.setForeground(UIManager.getColor("RadioButtonMenuItem.foreground"));
-		frmServerMangement.getContentPane().setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		frmServerMangement.getContentPane().setForeground(UIManager.getColor("RadioButtonMenuItem.acceleratorSelectionForeground"));
-		frmServerMangement.setTitle("Server Mangement");
-		frmServerMangement.setResizable(false);
-		frmServerMangement.setBounds(200, 200, 730, 686);
-		frmServerMangement.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmServerMangement.getContentPane().setLayout(null);
-		frmServerMangement.setBackground(Color.ORANGE);
-
-		JLabel lblIP = new JLabel("IP ADDRESS");
-		lblIP.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblIP.setBounds(26, 120, 89, 16);					////// Vi tri lbl IP
-		frmServerMangement.getContentPane().add(lblIP);
-
+		// Create main frame
+		frServer = new JFrame();
+		frServer.getContentPane().setLayout(null);
+		frServer.setForeground(UIManager.getColor("RadioButtonMenuItem.foreground"));
+		frServer.getContentPane().setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		frServer.getContentPane().setForeground(UIManager.getColor("RadioButtonMenuItem.acceleratorSelectionForeground"));
+		frServer.setTitle("Chat Server");
+		frServer.setResizable(false);
+		frServer.setBounds(100, 100, 350, 540);
+		frServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frServer.getContentPane().setBackground(new Color(253, 219, 191));
+		
+		// Create IP Address field
+		JLabel lbIPaddr = new JLabel("IP ADDRESS");
+		lbIPaddr.setForeground(Color.BLUE);
+		lbIPaddr.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lbIPaddr.setBounds(26, 60, 89, 16);
+		frServer.getContentPane().add(lbIPaddr);
+		
 		txtIP = new JTextField();
-		txtIP.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		txtIP.setFont(new Font("Segoe UI", Font.PLAIN, 15));
 		txtIP.setEditable(false);
-		txtIP.setBounds(126, 114, 176, 28);				////// Vi tri text Ip
-		frmServerMangement.getContentPane().add(txtIP);
-		txtIP.setColumns(10);
+		txtIP.setBounds(126, 55, 160, 30);		
+		frServer.getContentPane().add(txtIP);
+		txtIP.setColumns(12);
 		try {
 			txtIP.setText(Inet4Address.getLocalHost().getHostAddress());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-
-		JLabel lblNewLabel = new JLabel("PORT");
-		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblNewLabel.setBounds(428, 120, 48, 16);			////// Vi tri lbl Port
-		frmServerMangement.getContentPane().add(lblNewLabel);
-
-		txtPort = new JTextField();
-		txtPort.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		txtPort.setEditable(false);
-		txtPort.setColumns(10);
-		txtPort.setBounds(488, 114, 224, 28);
-		frmServerMangement.getContentPane().add(txtPort);			///// Vi tri cua text Port
-		txtPort.setText("8080");
-
-		JButton btnStart = new JButton("START");
-		btnStart.setBackground(UIManager.getColor("RadioButtonMenuItem.selectionBackground"));
-		btnStart.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		
-		btnStart.setBounds(416, 155, 143, 43);			/////// Vi tri button START
-		frmServerMangement.getContentPane().add(btnStart);
-		btnStart.setIcon(new javax.swing.ImageIcon(ServerGUI.class.getResource("/image/start.png")));
+		// Create STATUS field
+		JLabel lbNewStates = new JLabel("STATUS");
+		lbNewStates.setForeground(Color.BLUE);
+		lbNewStates.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lbNewStates.setBounds(26, 100, 89, 16);
+		frServer.getContentPane().add(lbNewStates);
 		
-//		BufferedImage img = null;
-//		try {
-//		    img = ImageIO.read(new File(ServerGUI.class.getResource("/image/serverManager.png").getFile()));
-//		} catch (IOException e) {
-//		    e.printStackTrace();
-//		}
-//		Image dimg = img.getScaledInstance(64, 64,
-//		        Image.SCALE_SMOOTH);
-//		ImageIcon imageIcon = new ImageIcon(dimg);
+		lbStates = new JLabel("Server States");
+		lbStates.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lbStates.setBounds(126, 100, 98, 16);
 
+		frServer.getContentPane().add(lbStates);
+		lbStates.setText("<html><font color='red'>OFF</font></html>");
 		
-		JLabel lblNhom = new JLabel("Server Management");
-		lblNhom.setFont(new Font("Segoe UI", Font.PLAIN, 18));
-		lblNhom.setBounds(169, 13, 268, 76);
-		lblNhom.setIcon(new javax.swing.ImageIcon(ServerGUI.class.getResource("/images/servermanager64x64.png")));
-		frmServerMangement.getContentPane().add(lblNhom);
-
+		// Create USER ONLINE number field
+		JLabel lbOnliner = new JLabel("USER ON");
+		lbOnliner.setForeground(Color.BLUE);
+		lbOnliner.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lbOnliner.setBounds(26, 140, 89, 16);
+		frServer.getContentPane().add(lbOnliner);
+				
+		lbUserOnl = new JLabel("0");
+		lbUserOnl.setForeground(Color.BLUE);
+		lbUserOnl.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		lbUserOnl.setBounds(126, 140, 56, 16);
+		frServer.getContentPane().add(lbUserOnl);
+		
+		// Create USER ONLINE name field
 		txtMessage = new TextArea();					
-		txtMessage.setBackground(Color.BLACK);
-		txtMessage.setForeground(Color.GREEN);
-		txtMessage.setFont(new Font("Consolas", Font.PLAIN, 14));
+		txtMessage.setBackground(Color.WHITE);
+		txtMessage.setForeground(Color.BLUE);
+		txtMessage.setFont(new Font("Consolas", Font.BOLD, 15));
 		txtMessage.setEditable(false);
-		txtMessage.setBounds(0, 267, 714, 358);		////// Vi tri textArea
-		frmServerMangement.getContentPane().add(txtMessage);
-
-		JButton btnStop = new JButton("STOP");
+		txtMessage.setBounds(26, 180, 286, 237);		////// Vi tri textArea
+		frServer.getContentPane().add(txtMessage);
+		
+		// Create STOP button
+		JButton btnStop = new JButton("");
+		btnStop.setBackground(UIManager.getColor("RadioButtonMenuItem.selectionBackground"));
 		btnStop.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		btnStop.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				lblUserOnline.setText("0");
 				try {
 					server.stopserver();
 					ServerGUI.updateMessage("STOP SERVER");
-					lblStatus.setText("<html><font color='red'>OFF</font></html>");
+					lbStates.setText("<html><font color='red'>OFF</font></html>");
 				} catch (Exception e) {
 					e.printStackTrace();
 					ServerGUI.updateMessage("STOP SERVER");
-					lblStatus.setText("<html><font color='red'>OFF</font></html>");
+					lbStates.setText("<html><font color='red'>OFF</font></html>");
 				}
 			}
 		});
-		btnStop.setBounds(571, 155, 143, 43);						//// Vi tri button Stop
-		frmServerMangement.getContentPane().add(btnStop);
-		btnStop.setIcon(new javax.swing.ImageIcon(ServerGUI.class.getResource("/image/stop.png")));
+		btnStop.setBounds(212, 437, 44, 44);
+		btnStop.setIcon(new javax.swing.ImageIcon(ServerGUI.class.getResource("/images/stop.png")));
+		frServer.getContentPane().add(btnStop);
+				
+		// Create START button
+		JButton btnStart = new JButton("");
+		btnStart.setBackground(UIManager.getColor("RadioButtonMenuItem.selectionBackground"));
+		btnStart.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		
-		JLabel lblnew111 = new JLabel("STATUS");
-		lblnew111.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblnew111.setBounds(26, 168, 89, 16);
-		frmServerMangement.getContentPane().add(lblnew111);
-		
-		lblStatus = new JLabel("New label");
-		lblStatus.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblStatus.setBounds(126, 168, 98, 16);
-
-
-		frmServerMangement.getContentPane().add(lblStatus);
-		lblStatus.setText("<html><font color='red'>OFF</font></html>");
-		
-		JLabel lblRecord = new JLabel("LOG");
-		lblRecord.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblRecord.setBounds(26, 245, 89, 16);
-		frmServerMangement.getContentPane().add(lblRecord);
-		
-		JLabel lbllabelUserOnline = new JLabel("USER ONLINE");
-		lbllabelUserOnline.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lbllabelUserOnline.setBounds(26, 207, 89, 16);
-		frmServerMangement.getContentPane().add(lbllabelUserOnline);
-		
-		lblUserOnline = new JLabel("0");
-		lblUserOnline.setForeground(Color.BLUE);
-		lblUserOnline.setFont(new Font("Segoe UI", Font.PLAIN, 13));
-		lblUserOnline.setBounds(126, 207, 56, 16);
-		frmServerMangement.getContentPane().add(lblUserOnline);
-		
-		JMenuBar menuBar = new JMenuBar();
-		frmServerMangement.setJMenuBar(menuBar);
-		
-		JMenu mnNewMenu = new JMenu("About");
-		menuBar.add(mnNewMenu);
-		
-		JMenuItem mntmMe = new JMenuItem("Us");
-		mnNewMenu.add(mntmMe);
-		
-		JMenuItem mntmSoftware = new JMenuItem("Software");
-		mnNewMenu.add(mntmSoftware);
 		btnStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -222,13 +149,39 @@ public class ServerGUI {
 					server = new Server(8080);
 					
 					ServerGUI.updateMessage("START SERVER");
-					lblStatus.setText("<html><font color='green'>RUNNING...</font></html>");
+					lbStates.setText("<html><font color='green'>ON</font></html>");
 				} catch (Exception e) {
 					ServerGUI.updateMessage("START ERROR");
 					e.printStackTrace();
 				}
 			}
 		});
-	}
-}
 
+		btnStart.setBounds(94, 437, 44, 44);			/////// Vi tri button START
+		btnStart.setIcon(new javax.swing.ImageIcon(ServerGUI.class.getResource("/images/start.png")));
+		frServer.getContentPane().add(btnStart);
+	}
+	
+	public ServerGUI() {
+		initialize();
+	}
+	
+	public static String getLabelUserOnl() {
+		return lbUserOnl.getText();
+	}
+	
+	public static void incrNumOfClients() {
+		int num = Integer.parseInt(lbUserOnl.getText());
+		lbUserOnl.setText(Integer.toString(num + 1));
+	}
+	
+	public static void decrNumOfClients() {
+		int num = Integer.parseInt(lbUserOnl.getText());
+		lbUserOnl.setText(Integer.toString(num - 1));
+	}
+	
+	public static void updateMessage(String msg) {
+		txtMessage.append(msg + "\n");
+	}
+
+}
